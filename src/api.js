88c1457ad98bedd1e2e3fd4a0dbded425b8b3c28
André Perdigão <andrepcg@gmail.com>
2017-02-02 19:21:16 +0000

    Query by location

--- src/api.js
+++ src/api.js
@@ -17,15 +17,32 @@ api.get('/day',
     };
   });
 
-api.post('/:collection',
+api.get('/find',
   async (ctx, next) => {
-    const { collection } = ctx.params;
-    const count = await ctx
-      .state
-      .collections[collection]
-      .add(ctx.request.body);
+    const { lat, long, radius } = ctx.query;
+    // const count = await ctx
+    //   .state
+    //   .collections[collection]
+    //   .add(ctx.request.body);
+    //   
+    
+    const query = {
+      geometry: {
+        $near : {
+          $geometry: {
+            type: "Point",
+            coordinates: [Number(lat), Number(long)]
+          },
+          $maxDistance: 2000
+        }
+      }
+    }
 
-    ctx.status = 201;
+    const results = await earthquakes.find(query);
+
+    ctx.body = {
+      results
+    }
   });
 
 export default api;
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Koa from 'koa';
 import api from './api';
 import bodyParser from 'koa-bodyparser';
+import json from 'koa-json';
 // import cors from 'kcors';
 
 const app = new Koa()
@@ -10,6 +11,7 @@ const app = new Koa()
   //   ctx.state.authorizationHeader = 'Key ' + config.key;
   //   await next();
   // })
+  .use(json())
   .use(bodyParser())
   .use(api.routes())
   .use(api.allowedMethods());
