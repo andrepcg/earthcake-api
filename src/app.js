f57bbb07018adb7ef4b2c4d25c7099d8f162b512
André Perdigão <andrepcg@gmail.com>
2017-02-03 12:27:44 +0000

    Home page

--- src/app.js
+++ src/app.js
@@ -3,18 +3,21 @@ import api from './api';
 import bodyParser from 'koa-bodyparser';
 import json from 'koa-json';
 import cors from 'kcors';
+import KoaRouter from 'koa-router';
 
 const isProduction = process.env.NODE_ENV === "production";
 
+const home = KoaRouter();
+home.get('/', async (ctx, next) => {
+  ctx.body = "Hello World";
+});
+
 const app = new Koa()
   .use(cors())
-  // .use(async (ctx, next) => {
-  //   ctx.state.collections = config.collections;
-  //   ctx.state.authorizationHeader = 'Key ' + config.key;
-  //   await next();
-  // })
   .use(json({ pretty: !isProduction }))
   .use(bodyParser())
+  .use(home.routes())
+  .use(home.allowedMethods())
   .use(api.routes())
   .use(api.allowedMethods());
 
