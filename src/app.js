dc3e445d4b6fb9b761c480dc4f94b77d9f105599
André Perdigão <andrepcg@gmail.com>
2017-02-06 23:27:45 +0000

    Frontend!

--- src/app.js
+++ src/app.js
@@ -4,21 +4,28 @@ import bodyParser from 'koa-bodyparser';
 import json from 'koa-json';
 import cors from 'kcors';
 import KoaRouter from 'koa-router';
+import serve from 'koa-static';
+import send from 'koa-send';
 
 const isProduction = process.env.NODE_ENV === "production";
 
 const home = KoaRouter();
 home.get('/', async (ctx, next) => {
   ctx.body = "Hello World";
+  // await send(ctx, '/public/index.html');
 });
 
 const app = new Koa()
+  .use(serve(__dirname + "/public"))
   .use(cors())
   .use(json({ pretty: !isProduction }))
   .use(bodyParser())
   .use(home.routes())
   .use(home.allowedMethods())
   .use(api.routes())
-  .use(api.allowedMethods());
+  .use(api.allowedMethods())
+  .use(async (ctx, next) => {
+    ctx.redirect('/');
+  });
 
 export default app;
